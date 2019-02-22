211f1a14c00c5eb7d0c8d9491ea59bf0d6a5a676
DevY <hanys1042@gmail.com>
2019-02-22 15:16:22 +0900

    first commit

---
+++ src/main/java/devy/temp/spring/sqlite/SpringSqliteApplication.java
@@ -0,0 +1,13 @@
+package devy.temp.spring.sqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSqliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSqliteApplication.class, args);
+	}
+
+}
